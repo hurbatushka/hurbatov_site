dce3281a73197af1b7c4373f3b00a8d3669cecb2
Иван Хурбатов <hurbatushka@gmail.com>
2023-10-31 15:00:39 +0300

    logos

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,18 @@
-'use client';
 import Image from 'next/image';
-import * as React from 'react';
-import { useEffect, useState } from 'react';
-
-type DarkModeChangeEvent = MediaQueryListEvent & {
-  matches: boolean;
-};
 
 export default function Home() {
-  const [isDarkMode, setIsDarkMode] = useState(
-    window.matchMedia('(prefers-color-scheme: dark)').matches,
-  );
-
-  useEffect(() => {
-    const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-    const handleDarkModeChange = (e: DarkModeChangeEvent) => {
-      setIsDarkMode(e.matches);
-    };
-    darkModeMediaQuery.addEventListener('change', handleDarkModeChange);
-    return () => {
-      darkModeMediaQuery.removeEventListener('change', handleDarkModeChange);
-    };
-  }, []);
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="mb-20 relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        {/* <h1 className={`mb-5 text-6xl font-semibold`}>HURBATOV.BY</h1> */}
-        {isDarkMode ? (
-          <Image src="/logo-hurbato.svg" width={450} height={450} alt="hurbato" />
-        ) : (
-          <Image src="/logo-hurbato_dark.svg" width={450} height={450} alt="hurbato" />
-        )}
+        <Image src="/logo-hurbato.svg" width={450} height={450} alt="hurbato" />
       </div>
+      <p className="mr-2 ml-2 font-semibold text-center prose font-black">
+        Скоро тут будет волшебный проект!
+      </p>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Тут когда-то будет мой сайт на Next, а пока тут просто будет&nbsp;
-          <code className="font-mono font-bold">это</code>
-        </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           От
-          <h1 className={`mr-2 ml-2 mt-11 text-2xl font-semibold`}>Hurbatov</h1>
+          <h1 className={`mr-2 ml-2 text-3xl font-semibold`}>Hurbatov</h1>
         </div>
       </div>
     </main>
